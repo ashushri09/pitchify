3d01568053a3d2f198d5be3cd20bdaf76b547fc0
a0s0u33 <ashutosh.shrivastava@walmart.com>
2026-09-17 00:23:32 +0530

    fix: import StartupTypeCard in UserStartups

--- components/UserStartups.tsx
+++ components/UserStartups.tsx
@@ -1,6 +1,6 @@
 import { client } from "@/sanity/lib/client";
 import { STARTUPS_BY_AUTHOR_QUERY } from "@/sanity/lib/queries";
-import StartupCard from "@/app/components/StartupCard";
+import StartupCard, { StartupTypeCard } from "@/app/components/StartupCard";
 
 const UserStartups = async ({ id }: { id: string }) => {
   const startups = await client.fetch(STARTUPS_BY_AUTHOR_QUERY, { id });
